f303dfaa7da94522ebf66c2ad3c2942b85c066fc
jonathanbayle <jonathan.bayle002@gmail.com>
2024-02-14 11:56:44 +0100

    Ajout du code du cours et suppression de l'historique

--- src/StateMachineManipulation.java
+++ src/StateMachineManipulation.java
@@ -22,7 +22,7 @@ public class StateMachineManipulation {
 			resource.getContents().add(root);
 			resource.save(null);
 		} catch (Exception e) {
-			System.err.println("ERREUR sauvegarde du modele : "+e);
+			System.err.println("ERREUR sauvegarde du modele : " + e);
 			e.printStackTrace();
 		}
 	}
@@ -38,9 +38,8 @@ public class StateMachineManipulation {
 			java.util.Map<String, XMLMap> options = new java.util.HashMap<String, XMLMap>();
 			options.put(XMLResource.OPTION_XML_MAP, xmlMap);
 			resource.load(options);
-		}
-		catch(Exception e) {
-			System.err.println("ERREUR chargement du modele : "+e);
+		} catch (Exception e) {
+			System.err.println("ERREUR chargement du modele : " + e);
 			e.printStackTrace();
 		}
 		return resource;
@@ -56,7 +55,7 @@ public class StateMachineManipulation {
 		TreeIterator<EObject> it = resource.getAllContents();
 
 		StateMachine sm = null;
-		while(it.hasNext()) {
+		while (it.hasNext()) {
 			EObject obj = it.next();
 			if (obj instanceof StateMachine) {
 				sm = (StateMachine) obj;
@@ -66,8 +65,115 @@ public class StateMachineManipulation {
 		return sm;
 	}
 
-	
-	
+	// Initialise la machine à états en activant tous les états initiaux
+	public void initStateMachine(StateMachine sm) {
+		sm.setIsActive(true);
+		State s = sm.getInitialState().getReferencedState();
+		while (s != null) {
+			s.setIsActive(true);
+			if (s instanceof CompositeState)
+				s = ((CompositeState) s).getInitialState().getReferencedState();
+			else
+				s = null;
+		}
+	}
+
+	// Désactive la hiérarchie haute d'un état
+	public void unactivateUpStateHierarchy(State s) {
+		State up = s.getContainer();
+		while (up != null) {
+			up.setIsActive(false);
+			up = up.getContainer();
+		}
+	}
+
+	// Désactive la hiérarchie basse d'un état
+	public void unactivateDownStateHierarchy(State s) {
+		if (s instanceof CompositeState)
+			for (State down : ((CompositeState) s).getStates()) {
+				down.setIsActive(false);
+				if (down instanceof CompositeState)
+					this.unactivateDownStateHierarchy(down);
+			}
+	}
+
+	// Désactive un état et toute sa hiérarchie
+	public void unactivateStateHierarchy(State s) {
+		s.setIsActive(false);
+		this.unactivateUpStateHierarchy(s);
+		this.unactivateDownStateHierarchy(s);
+	}
+
+	// Active la hiérarchie haute d'un état
+	public void activateUpStateHierarchy(State s) {
+		State up = s.getContainer();
+		while (up != null) {
+			up.setIsActive(true);
+			up = up.getContainer();
+		}
+	}
+
+	// Active la hiérarchie basse d'un état
+	public void activateDownStateHierarchy(State s) {
+		if (s instanceof CompositeState) {
+			State init = ((CompositeState) s).getInitialState().getReferencedState();
+			init.setIsActive(true);
+			if (init instanceof CompositeState)
+				this.activateDownStateHierarchy(init);
+		}
+	}
+
+	// Active toute la hiérarchie d'un état
+	public void activateStateHierarchy(State s) {
+		s.setIsActive(true);
+		this.activateUpStateHierarchy(s);
+		this.activateDownStateHierarchy(s);
+	}
+
+	// Retourne l'état actif le plus en bas de la hiérarchie
+	public State getLeafActiveState(CompositeState comp) {
+		for (State s : comp.getStates())
+			if (s.isIsActive())
+				if (s instanceof CompositeState)
+					return this.getLeafActiveState((CompositeState) s);
+				else
+					return s;
+		return null;
+	}
+
+	// Retourne la transition partant de l'état actif le plus bas pour l'événement
+	// précisé ou null si aucune transition n'a été trouvée
+	public Transition getTriggerableTransition(String evt, StateMachine sm) {
+		boolean fini = false;
+		State activeState = this.getLeafActiveState(sm);
+		Transition trans = null;
+		while (!fini) {
+			for (Transition t : sm.getTransitions())
+				if (t.getEvent().getName().equals(evt) && (activeState == t.getSource())) {
+					trans = t;
+					fini = true;
+				}
+			if (!fini) {
+				activeState = activeState.getContainer();
+				if (activeState == null)
+					fini = true;
+			}
+		}
+		return trans;
+	}
+
+	// Traite un événement : recherche une transition à suivre puis si elle
+	// existe, désactive la hiérarchie de l'état source puis modifie
+	// la hiérarchie des états actifs à partir de la cible de la transition
+	// en prenant en compte le cas où la cible est un état historique
+	public void processEvent(String event, StateMachine sm) {
+		Transition trans = this.getTriggerableTransition(event, sm);
+		if (trans != null) {
+			this.unactivateStateHierarchy(trans.getSource());
+			State target = trans.getTarget();
+			this.activateStateHierarchy(target);
+		}
+	}
 
 	public static void main(String argv[]) {
 
@@ -76,7 +182,7 @@ public class StateMachineManipulation {
 		System.out.println(" Chargement du modele");
 
 		StateMachine model = sm.getModelBase("models/Feu.xmi");
-		
+
 		System.out.println(" Modele charge");
 	}
 }
